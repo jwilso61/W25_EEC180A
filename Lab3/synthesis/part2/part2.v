103e76dd1633042e6478814daaa2d39161c04b5f
jwilso61 <jwilso@ucdavis.edu>
2025-01-26 19:19:05 -0800

    Update part2.v

--- Lab3/synthesis/part2/part2.v
+++ Lab3/synthesis/part2/part2.v
@@ -4,38 +4,40 @@
 //=======================================================
 
 module part2(
-
-	//////////// SEG7 //////////
-	output		     [7:0]		HEX0,
-	output		     [7:0]		HEX1,
-	output		     [7:0]		HEX2,
-	output		     [7:0]		HEX3,
-	output		     [7:0]		HEX4,
-	output		     [7:0]		HEX5,
-
-	//////////// KEY //////////
-	input 		     [1:0]		KEY,
-
 	//////////// LED //////////
-	output		     [9:0]		LEDR,
+	output		     [3:0]		LEDR,
 
 	//////////// SW //////////
-	input 		     [9:0]		SW
+	input 		     [7:0]		SW
 );
-
 
 
 //=======================================================
 //  REG/WIRE declarations
 //=======================================================
 
-
-
+reg carryIn = 1'b0;
+wire overflow;
+wire [2:0] lzd4_1Count;
+wire [2:0] lzd4_0Count;
+wire [2:0] LeadNibOvr;
 
 //=======================================================
 //  Structural coding
 //=======================================================
+ 
+LZD_4 lzd4_1 (lzd4_1Count[2:0], SW[7:4]);
+LZD_4 lzd4_0 (lzd4_0Count[2:0], SW[3:0]);
 
+assign LeadNibOvr[2:0] = (lzd4_1Count[2] == 1) 	? (lzd4_0Count[2:0])
+																: (3'b000);
 
+part3 #(.N(4)) FA4 (
+	.s(LEDR[3:0]), 
+	.overflow(overflow), 
+	.carryIn(carryIn), 
+	.a(lzd4_1Count[2:0]), 
+	.b(LeadNibOvr[2:0])
+);
 
 endmodule
