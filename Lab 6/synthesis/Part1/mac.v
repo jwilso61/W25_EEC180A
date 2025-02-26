b04dfd4e65c20e8607df73151ce6c2fc7da776d8
thomtrin <ttttrinh@ucdavis.edu>
2025-02-26 13:06:05 -0800

    Lab 6 task 1

---
+++ Lab 6/synthesis/Part1/mac.v
@@ -0,0 +1,23 @@
+module MAC (
+	input clk,
+	input macc_clear,
+	input signed [7:0] inA,
+	input signed [7:0] inB,
+	output reg [18:0] out
+	);
+	
+
+	reg signed [18:0] S ;
+	reg signed [18:0] product;
+	
+always @ (posedge clk) begin
+
+	 product = inA * inB;
+	 if (macc_clear == 1) begin
+		S = product;
+	 end else begin
+		S = S + product; 
+	end 
+	out <= S;
+end 	
+endmodule 
